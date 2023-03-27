616cb3309292d72903940fb236edaacfedfac524
Christian Seyoum <chriscoding96@gmail.com>
2023-03-27 18:20:52 -0400

    Create My Portfolio

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: "About",
+        link: "#about",
+        cName: "nav-links"
+    },
+    {
+        title: "Skills",
+        link: "#skill",
+        cName: "nav-links"
+    },
+    {
+        title: "Education",
+        link: "#education",
+        cName: "nav-links"
+    },
+    {
+        title: "Projects",
+        link: "#project",
+        cName: "nav-links"
+    }
+]
