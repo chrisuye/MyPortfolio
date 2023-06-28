bbe5636977102a1f176f07a15533e2c9c4afca81
Christian Seyoum <chriscoding96@gmail.com>
2023-06-28 03:57:35 +0000

    Update in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+      Update In Progress
     </div>
   );
 }
