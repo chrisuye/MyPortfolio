96542c082867934160835b8392fe94368febbdd8
Chris <chriscoding96@gmail.com>
2024-06-29 14:52:57 -0400

    Update done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      Update In Progress
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
